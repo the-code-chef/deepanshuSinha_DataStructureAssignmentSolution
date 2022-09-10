f339a22dba29d5ecabebfc503eb5b66e61ef7bb7
Deepanshu Sinha <107sinha@gmail.com>
2022-09-10 18:30:44 +0530

    separated input method in skyscrapper

--- src/com/gl/dataStructureAssignment/skyscraper/FloorGenerator.java
+++ src/com/gl/dataStructureAssignment/skyscraper/FloorGenerator.java
@@ -1,8 +1,28 @@
 package com.gl.dataStructureAssignment.skyscraper;
 
+import java.util.Scanner;
+
 public class FloorGenerator {
 
-    void calculateFloors(int[] arr, int num) {
+    int[] arr;
+    int num;
+
+    void insertFloors() {
+        System.out.println("Enter the total no of floors in the building: ");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+
+        arr = new int[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Enter the floor size given on day : " + i);
+            int n = sc.nextInt();
+            arr[n] = i;
+        }
+        sc.close();
+    }
+
+    void calculateFloors() {
         int j = num;
         boolean flag;
 
